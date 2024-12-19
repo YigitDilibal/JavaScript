3b8594e3dd2a433464ef42f16286f5cd11f83df2
YigitDilibal <archery10@outlook.com>
2024-12-19 11:23:04 +0200

    first commit

---
+++ p01.js
@@ -0,0 +1,34 @@
+console.log('Hello World')
+
+console.log("Hello")
+
+var isim='Yigit'
+let soyisim = "Dilibal"
+var age = 25
+var isEmekli = false
+
+const pi = 3.14 /* degistirilemez variable*/
+
+var number, job, city
+number=12
+job="IT"
+city="Sofia"
+age=25
+
+console.log(age, pi, city, job,number,isim,soyisim)
+console.log("----------------------------------")
+
+var person ={
+
+    namePerson : "Yigit",
+    surnamePerson : "Dilibal",
+    numberPerson : 250
+}
+
+console.log(person)
+console.log(person.namePerson)
+console.log(person.namePerson + " " + person.surnamePerson,person.numberPerson)
+
+var sayi = 23
+if(sayi<20)
+console.log("buyuk")
